ce597f92c3e9bb09827fd312f8dd9f1729f7dd01
Neale Taylor <nealeptaylor@gmail.com>
2022-11-01 14:01:01 -0400

    remove comma

--- server/db/schema/03_create_booked.sql
+++ server/db/schema/03_create_booked.sql
@@ -1,9 +1,9 @@
 -- schema/03_create_booked.sql
 DROP TABLE IF EXISTS bookedDays CASCADE;
 -- CREATE URLS
-CREATE TABLE urls (
+CREATE TABLE bookedDays (
   id SERIAL PRIMARY KEY,
   day integer NOT NULL,
   month integer NOT NULL,
-  year integer NOT NULL,
+  year integer NOT NULL
 );
