0a84a184b25a4ee2feba716c3e2fc8875170b3fc
Neale Taylor <nealeptaylor@gmail.com>
2022-11-01 12:39:24 -0400

    establish axios route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,15 @@
 import './App.css';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
 
 function App() {
+  const [data, setData] = useState("nothing");
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/users').then(res => {
+      console.log(res);
+    })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
