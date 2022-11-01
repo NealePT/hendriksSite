12a4559410ed6d08c01926d1721783bf4b2a3192
Neale Taylor <nealeptaylor@gmail.com>
2022-11-01 14:47:03 -0400

    move code to useappdata

--- client/src/hooks/useAppData.js
+++ client/src/hooks/useAppData.js
@@ -6,13 +6,20 @@ export default function useAppData() {
 
   const [state, setState] = useState({
     users: [],
-    bookedDays: []
+    bookedDays: [],
   });
 
   useEffect(() => {
     Promise.all([
       axios.get('/users'),
       axios.get('/bookedDays')
-    ]).then(/**/)
-  })
+    ]).then((all) => {
+      const [users, bookedDays] = all;
+      setState(prev => ({...prev, users: users.data.users, bookedDays: bookedDays.data.bookedDays}))
+    })
+  }, [])
+
+  appData.state = state;
+
+  return appData;
 }
