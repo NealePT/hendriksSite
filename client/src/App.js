fe30a3fb747193c206115346ecf07672a99a0fa2
Neale Taylor <nealeptaylor@gmail.com>
2022-11-01 12:46:17 -0400

    tinker with display of data

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,16 @@ function App() {
   const [data, setData] = useState("nothing");
 
   useEffect(() => {
-    axios.get('http://localhost:8080/users').then(res => {
-      console.log(res);
+    axios.get('http://localhost:8080/users')
+    .then(res => {
+      console.log(res.data.users[0]) 
+      setData(res.data.users[0].email)
     })
   }, [])
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <h1>Hello World</h1>
+      <p>{!data ? "Loading..." : data}</p>
     </div>
   );
 }
