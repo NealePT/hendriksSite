ceca4d0fc2302748cfa53d483f8efc4fd2113c33
Neale Taylor <nealeptaylor@gmail.com>
2022-11-01 13:39:47 -0400

    modify promises

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,32 @@
 import './App.css';
 import axios from 'axios';
-import {useState, useEffect} from 'react';
+import {React, useState, useEffect} from 'react';
 
 function App() {
-  const [data, setData] = useState("nothing");
+  const [data, setData] = useState("");
+  const [state, setState] = useState({
+    users: [],
+    bookedDays: [],
+  });
 
   useEffect(() => {
-    axios.get('http://localhost:8080/users')
-    .then(res => {
-      console.log(res.data.users[0]) 
-      setData(res.data.users[0].email)
+    Promise.all([
+      axios.get('/users')
+    ])
+    .then((all) => {
+      console.log(all[0].data.users)
+      setState(prev => ({
+        ...prev,
+        users: all[0].data.users
+      }))
     })
   }, [])
+
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <p>{!data ? "Loading..." : data}</p>
+      <p>{!state.users[0] ? "Loading..." : state.users[0].email}</p>
+      {console.log(state.users)}
     </div>
   );
 }
