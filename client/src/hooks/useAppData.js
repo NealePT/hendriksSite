ab9c52b291636118c8cb06b36d0cf2b32c866560
Neale Taylor <nealeptaylor@gmail.com>
2022-11-01 13:39:57 -0400

    add content to useappdata

---
+++ client/src/hooks/useAppData.js
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function useAppData() {
+  //Default states 
+  const [state, setState] = useState({
+    users: [],
+    bookedDays: []
+  });
+}
