f6edb885079a66dc6f3015a4585052f21310546b
Neale Taylor <nealeptaylor@gmail.com>
2022-11-03 13:01:53 -0400

    import navbar to app

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import axios from 'axios';
 import {React, useState, useEffect} from 'react';
 import useAppData from './hooks/useAppData'
+import NavBar from './components/NavBar';
+
 
 function App() {
   const { state } = useAppData();
@@ -13,6 +15,7 @@ console.log(state)
 
   return (
     <main className="App">
+      <NavBar />
       <h1>Hello World</h1>
       {userList}
     </main>
