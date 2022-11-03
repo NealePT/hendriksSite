a8dd8413d6846030924c6c394a3154ee9de0feea
Neale Taylor <nealeptaylor@gmail.com>
2022-11-03 13:10:58 -0400

    add links to navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,7 +4,12 @@ import "./NavBar.css";
 export default function NavBar(props) {
   return (
     <div className="NavBar">
-      
+      <ul className="navLinks">
+        <li><a href="#summary">Summary</a></li>
+        <li><a href="#photos">Photos</a></li>
+        <li><a href="#ammenities">Amenities</a></li>
+        <li><a href="#location">Location</a></li>
+      </ul>
     </div>
   )
 };
