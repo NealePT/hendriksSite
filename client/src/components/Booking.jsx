3938af4254fd5f7d82f8698cd1d5ca4863e7ab15
Neale Taylor <nealeptaylor@gmail.com>
2022-11-07 17:48:40 -0500

    change booking component

--- client/src/components/Booking.jsx
+++ client/src/components/Booking.jsx
@@ -9,78 +9,28 @@
 //   )
 // };
 
-import React from 'react';
+import {useState} from 'react';
 import './Booking.css';
 import dayjs from 'dayjs';
-import isBetweenPlugin from 'dayjs/plugin/isBetween';
-import { styled } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
-import { PickersDay } from '@mui/x-date-pickers/PickersDay';
-import advancedFormat from 'dayjs/plugin/advancedFormat'
 
-const CustomPickersDay = styled(PickersDay, {
-  shouldForwardProp: (prop) =>
-    prop !== 'dayIsBetween' && prop !== 'isFirstDay' && prop !== 'isLastDay',
-})(({ theme, dayIsBetween, isFirstDay, isLastDay }) => ({
-  ...(dayIsBetween && {
-    borderRadius: 0,
-    backgroundColor: theme.palette.primary.main,
-    color: theme.palette.common.white,
-    '&:hover, &:focus': {
-      backgroundColor: theme.palette.primary.dark,
-    },
-  }),
-  ...(isFirstDay && {
-    borderTopLeftRadius: '50%',
-    borderBottomLeftRadius: '50%',
-  }),
-  ...(isLastDay && {
-    borderTopRightRadius: '50%',
-    borderBottomRightRadius: '50%',
-  }),
-}));
-
-export default function CustomDay() {
-  const [value, setValue] = React.useState(dayjs(new Date()));
-
-  const renderWeekPickerDay = (date, selectedDates, pickersDayProps) => {
-    if (!value) {
-      return <PickersDay {...pickersDayProps} />;
-    }
-
-    const start = value.startOf('week');
-    const end = value.endOf('week');
-
-    const dayIsBetween = date.isBetween(start, end, null, '[]');
-    const isFirstDay = date.isSame(start, 'day');
-    const isLastDay = date.isSame(end, 'day');
-
-    return (
-      <CustomPickersDay
-        {...pickersDayProps}
-        disableMargin
-        dayIsBetween={dayIsBetween}
-        isFirstDay={isFirstDay}
-        isLastDay={isLastDay}
-      />
-    );
-  };
+export default function Booking() {
+  const [value, setValue] = useState(() => dayjs(new Date()));
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <StaticDatePicker
-        displayStaticWrapperAs="desktop"
-        label="Week picker"
+        onChange={(newValue) => setValue(newValue)}
         value={value}
-        onChange={(newValue) => {
-          setValue(newValue);
-        }}
-        renderDay={renderWeekPickerDay}
         renderInput={(params) => <TextField {...params} />}
-        inputFormat="'Week of' MMM d"
+        componentsProps={{
+          actionBar: {
+            actions: ['today'],
+          },
+        }}
       />
     </LocalizationProvider>
   );
