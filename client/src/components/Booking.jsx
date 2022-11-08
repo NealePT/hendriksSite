38408432ed5eb62301429934aa41c83fce543f87
Neale Taylor <nealeptaylor@gmail.com>
2022-11-08 12:09:09 -0500

    add react datepicker

--- client/src/components/Booking.jsx
+++ client/src/components/Booking.jsx
@@ -1,89 +1,25 @@
-// import {useState} from 'react';
-// import './Booking.css';
-// import dayjs from 'dayjs';
-// import TextField from '@mui/material/TextField';
-// import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-// import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-// import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+import {useState} from 'react';
+import './Booking.css';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-// export default function Booking() {
-//   const [value, setValue] = useState(() => dayjs(new Date()));
-
-//   return (
-//     <LocalizationProvider dateAdapter={AdapterDayjs}>
-//       <StaticDatePicker
-//         onChange={(newValue) => setValue(newValue)}
-//         value={value}
-//         renderInput={(params) => <TextField {...params} />}
-//         componentsProps={{
-//           actionBar: {
-//             actions: ['today'],
-//           },
-//         }}
-//       />
-//     </LocalizationProvider>
-//   );
-// }
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import { LocalizationProvider } from '@mui/x-date-pickers-pro';
-import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
-import { StaticDateRangePicker } from '@mui/x-date-pickers-pro/StaticDateRangePicker';
-import { DateRangePickerDay as MuiDateRangePickerDay } from '@mui/x-date-pickers-pro/DateRangePickerDay';
-
-const DateRangePickerDay = styled(MuiDateRangePickerDay)(
-  ({
-    theme,
-    isHighlighting,
-    isStartOfHighlighting,
-    isEndOfHighlighting,
-    outsideCurrentMonth,
-  }) => ({
-    ...(!outsideCurrentMonth &&
-      isHighlighting && {
-        borderRadius: 0,
-        backgroundColor: theme.palette.primary.main,
-        color: theme.palette.common.white,
-        '&:hover, &:focus': {
-          backgroundColor: theme.palette.primary.dark,
-        },
-      }),
-    ...(isStartOfHighlighting && {
-      borderTopLeftRadius: '50%',
-      borderBottomLeftRadius: '50%',
-    }),
-    ...(isEndOfHighlighting && {
-      borderTopRightRadius: '50%',
-      borderBottomRightRadius: '50%',
-    }),
-  }),
-);
-
-export default function CustomDateRangePickerDay() {
-  const [value, setValue] = React.useState([null, null]);
-
-  const renderWeekPickerDay = (date, dateRangePickerDayProps) => {
-    return <DateRangePickerDay {...dateRangePickerDayProps} />;
+export default function Booking(props) {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(null);
+  const onChange = (dates) => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
   };
-
+  console.log(startDate, endDate)
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <StaticDateRangePicker
-        displayStaticWrapperAs="desktop"
-        label="date range"
-        value={value}
-        onChange={(newValue) => setValue(newValue)}
-        renderDay={renderWeekPickerDay}
-        renderInput={(startProps, endProps) => (
-          <React.Fragment>
-            <TextField {...startProps} />
-            <Box sx={{ mx: 2 }}> to </Box>
-            <TextField {...endProps} />
-          </React.Fragment>
-        )}
-      />
-    </LocalizationProvider>
+    <DatePicker
+      selected={startDate}
+      onChange={onChange}
+      startDate={startDate}
+      endDate={endDate}
+      selectsRange
+      inline
+    />
   );
-}
+};
