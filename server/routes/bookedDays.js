af54298dbd08c8f210ca69c3d2f77ca2aa1d251f
Neale Taylor <nealeptaylor@gmail.com>
2022-11-09 14:47:58 -0500

    add put route for days

--- server/routes/bookedDays.js
+++ server/routes/bookedDays.js
@@ -3,13 +3,29 @@ const express = require('express');
 const router = express.Router();
 const bookedDays = require('../db/queries/bookedDays');
 
-
-/* GET users listing. */
+// GET Booked Days listing. 
 router.get('/', (req, res) => {
   bookedDays.getAllBookedDays().then(data => {
     // console.log(data);
     res.json({bookedDays: data});
   });
 });
+
+// PUT /bookedDays/new
+
+module.exports = (db) => {
+  router.put('/new', (req, res) => {
+  // console.log('req.body =', req.body);
+    const { day, month, year } = req.body;
+    const values = [day, month, year];
+    const command = `
+      INSERT INTO bookedDays (day, month, year)
+      VALUES ($1, $2, $3)
+      RETURNING *;
+    `;
+    return db.query(command, values)
+      .then(data => res.send(data.rows[0]));
+  });
+};
 
 module.exports = router;
