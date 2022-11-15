b099d72d47b35ef380af5b7c1445bbe17dca11ee
Neale Taylor <nealeptaylor@gmail.com>
2022-11-15 16:09:36 -0500

    modify days schema

--- server/db/schema/03_create_booked.sql
+++ server/db/schema/03_create_booked.sql
@@ -4,6 +4,6 @@ DROP TABLE IF EXISTS bookedDays CASCADE;
 CREATE TABLE bookedDays (
   id SERIAL PRIMARY KEY,
   day integer NOT NULL,
-  month integer NOT NULL,
+  month VARCHAR(255) NOT NULL,
   year integer NOT NULL
 );
